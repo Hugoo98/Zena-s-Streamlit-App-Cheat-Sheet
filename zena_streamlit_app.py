8123d77448f82735d9e6fe51f90ec61668f4e04e
Omar Tachour <56323269+Hugoo98@users.noreply.github.com>
2023-11-21 10:35:41 +0100

    Update zena_streamlit_app.py

--- zena_streamlit_app.py
+++ zena_streamlit_app.py
@@ -1,3 +1,7 @@
+import streamlit
+import snowflake.connector
+import pandas
+
 # connect to snowflake
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
